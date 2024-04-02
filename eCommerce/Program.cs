ba20f6d75de08d9df981aabcde29a59625333a1b
Almantas Baltmiškis <almantas.baltmiskis@codeacademylt.onmicrosoft.com>
2024-04-02 19:08:34 +0300

    Create temp user object, and semifunctional check balance

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace eCommerce
-{
-    internal class Program
+﻿using eCommerce.Service;
+
+namespace eCommerce
+{    public class Program
     {
         static void Main(string[] args)
         {
+            {
+                Console.WriteLine("Hello, World!");
+                var list = new List<User>();
+                list.Add(new User("Alma", "password", 20.4));
+                list.Add(new User("Bob", "password", 75.2));
 
+
+
+                CheckBalanse.CheckBalanceNow(list);
+
+            }
         }
     }
 }
---
+++ eCommerce/Service/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public double Balance { get; set; }
+        public User()
+        {
+            Name = string.Empty;
+            Password = string.Empty;
+            Balance = 0;
+        }
+        public User(string name, string password, double balance)
+        {
+            Name = name;
+            Password = password;
+            Balance = balance;
+        }
+    }
+}
