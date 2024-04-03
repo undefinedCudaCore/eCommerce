bc46da692eedc18b99f0e0e7b7d99044af7b7915
undefinedCudaCore <teklithuania@gmail.com>
2024-04-03 18:41:24 +0300

    Add cart props

--- eCommerce/Models/Cart.cs
+++ eCommerce/Models/Cart.cs
@@ -1,6 +1,19 @@
-﻿namespace eCommerce.Models
+﻿using eCommerce.Models.ShopItem;
+
+namespace eCommerce.Models
 {
     internal class Cart
     {
+        public Cart()
+        {
+            CartItemList = new Dictionary<string, Item>();
+        }
+
+        private int _cartId { get; set; }
+        public Dictionary<string, Item> CartItemList { get; set; }
+
+        // To diplay in cart:
+        //Item -> Price -> Qty -> Total price
+        //Check, if car have few same item - collapse and append quantity
     }
 }
