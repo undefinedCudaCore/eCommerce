933f16f040f587e244a1832f989d63a8f05ebe74
Almantas Baltmiškis <almantas.baltmiskis@codeacademylt.onmicrosoft.com>
2024-04-03 18:31:16 +0300

    Cleanup

--- eCommerce/Service/CheckBalance.cs
+++ eCommerce/Service/CheckBalance.cs
@@ -16,16 +16,12 @@ namespace eCommerce.Service
             // Check which user is logged in and insert to method
 
             // Change method input stuff 
-
-            
-
-            // Testing Code
             string temp = "Alma";
-            var tempuser = User.Where(u => u.Name == temp).Select(u => u.Balance).ToList();
-            // Testing code
+
 
             foreach (User user in User)
             {
+                
                 if (temp == user.Name)
                 {
                     Console.WriteLine(user.Balance);
